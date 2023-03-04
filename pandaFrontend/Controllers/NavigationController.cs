37400d7ccb9b556ec5eba7df9bb121436e516fcb
Sherlock67 <alammushfiqul366@gmail.com>
2023-03-04 22:20:28 +0600

    api consumption

---
+++ pandaFrontend/Controllers/NavigationController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using pandacommerce_dal.Model;
+using System.Net.Http.Headers;
+
+namespace pandaFrontend.Controllers
+{
+    public class NavigationController : Controller
+    {
+        private static string url = "https://localhost:7175/";
+
+        public async Task<IActionResult> AllNavigationList()
+        {
+            List<NavCategory> listNav = new List<NavCategory>();
+            using (var client = new HttpClient())
+            {
+                client.DefaultRequestHeaders.Clear();
+                client.BaseAddress = new Uri(url);
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+                var response = await client.GetAsync("/api/v1/NavApiController/GetAllNavigationCategory");
+                if(response != null)
+                {
+                    var navigationlist = response.Content.ReadAsStringAsync().Result;
+                    listofnavmenu = JsonConvert.DeserializeObject<List<NavCategory>>(navigationlist);
+                }
+            }
+        return View(listNav); 
+        }
+
+
+    }
+}
