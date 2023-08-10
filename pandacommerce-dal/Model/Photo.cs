4304ea7b436cce99a2a4c7d1a40f2f2d80d0eb47
Sherlock67 <alammushfiqul366@gmail.com>
2023-08-10 14:29:54 +0600

    product picture adding functionality implementing

---
+++ pandacommerce-dal/Model/Photo.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pandacommerce_dal.Model
+{
+    public class Photo
+    {
+        public int Id { get; set; }
+        public string Url { get; set; }
+        public DateTime DateAdded { get; set; }
+        public Product Product { get; set; }
+        public int ProductId { get; set; }
+    }
+}
